3eb11c6d6d87a4a1bc978b9e8f6b3e27fb766d2d
Avansh Yadav <avanshyadav20072006@gmail.com>
2025-04-14 12:44:17 +0530

    REMOVED API firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZyC4-6A4RDY2jM6P7Sp_iVC8cMnJ5tFs",
-  authDomain: "prepmebuddy-2516f.firebaseapp.com",
-  projectId: "prepmebuddy-2516f",
-  storageBucket: "prepmebuddy-2516f.firebasestorage.app",
-  messagingSenderId: "468316661238",
-  appId: "1:468316661238:web:0dabebcb8601c29b35d3d7",
-  measurementId: "G-LPGKP388MP"
+  
 };
 
 const app = initializeApp(firebaseConfig);
